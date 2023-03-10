8f4478e46203bd8b3271c4878b96a19843c35109
templarec2 <templarec@gmail.com>
2023-03-10 19:37:41 +0100

    api rest with application password auth

--- README.txt
+++ README.txt
@@ -1,2 +1,16 @@
 estrarre i file e metterli in una cartella chiamata plugin-name
 spostare la cartella dentro wp-contents/plugins
+
+
+API REST:
+Creare una application password per l'utente in fondo alla pagina di impostazioni utente nella dashboard admin
+
+Usare l'endpoint POST /wp-json/logger/token
+con i paramentri: username, password, app_pass
+dove app_pass è la password generata sopra
+
+Viene restituito un token da aggiungere agli header per l'endpoint /wp-json/logger/logs
+
+Da aggiungere all'header: Authorization "Basic {token}"
+
+
--- admin/class-plugin-name-admin.php
+++ admin/class-plugin-name-admin.php
@@ -698,7 +698,26 @@
         
       );
       $logs = get_posts($args);
-      return $logs;
+      foreach ($logs as $log){
+        $log_datetime = get_post_meta($log->ID, $this->plugin_name.'_log_datetime', true );
+        $user_id = get_post_meta($log->ID, $this->plugin_name.'_user_id', true );
+        $user_ip = get_post_meta($log->ID, $this->plugin_name.'_user_ip', true );
+        $log_action = get_post_meta($log->ID, $this->plugin_name.'_log_datetime', true );
+        $log_metadata = get_post_meta($log->ID, $this->plugin_name.'_log_metadata', true );
+        $log_post_type = get_post_meta($log->ID, $this->plugin_name.'_log_post_type', true );
+        
+        $result = [
+            'data' => [
+                'log_datetime' => $log_datetime,
+                'user_id' => $user_id,
+                'user_ip' => $user_ip,
+                'log_post_type' => $log_post_type,
+                'log_action' => $log_action,
+                'log_metadata' => json_decode($log_metadata),
+            ]
+        ];
+      }
+      return $result;
   }
   }
   
